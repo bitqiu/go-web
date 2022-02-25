8c6f8915a8d910ec0522ede5c8f2731b1782157c
liulong@psylife.cc <dfsd534@qq.com>
2022-02-25 14:52:49 +0800

    hello world

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "go-web/cmd"
+import (
+	"github.com/gin-gonic/gin"
+	"go-web/cmd"
+	"net/http"
+)
 
 //go:generate swag init --parseDependency --parseDepth=6
 
@@ -17,5 +21,12 @@ func init() {
 }
 
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.AsciiJSON(http.StatusOK,gin.H{
+			"msg":"hello world!",
+		})
+	})
+	r.Run(":8000")
 	cmd.Execute()
 }
